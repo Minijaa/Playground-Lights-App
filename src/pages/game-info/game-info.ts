4ec11dad653cbbfc1988f5b2c0940d7806cbf00f
ASchub <ASchub@users.noreply.github.com>
2018-05-03 21:03:44 +0200

    Working Timer

--- src/pages/game-info/game-info.ts
+++ src/pages/game-info/game-info.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {NgModule} from '@angular/core';
+import {RoundProgressModule} from 'angular-svg-round-progressbar'
 
 /**
  * Generated class for the GameInfoPage page.
@@ -7,14 +9,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+@NgModule({
+  imports: [RoundProgressModule]
+})
 @IonicPage()
 @Component({
   selector: 'page-game-info',
   templateUrl: 'game-info.html',
 })
 export class GameInfoPage {
+  timer: any;
   game: any;
+  countdown: number=2;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.game = navParams.get('game')
@@ -26,6 +32,39 @@ export class GameInfoPage {
 
   setContent(game: any){
     this.game = game;
+  }
+
+  startGame(){
+
+  }
+
+  StartTimer() {
+    this.timer = setTimeout(x => {
+      // if (this.countdown <= 0) {
+      // }
+      if (this.countdown == 0){
+        this.countdown = 2;
+        this.getLogic();
+      }
+
+      this.countdown -= 1;
+
+      if (this.countdown > 0) {
+        this.StartTimer();
+      }
+
+
+
+    }, 1000);
+
+  }
+
+
+  getLogic(){
+
+    //här får vi trycka in något från Samuels logik
+    //game.logic
+
   }
 
 }
