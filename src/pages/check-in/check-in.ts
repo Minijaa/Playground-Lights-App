a1300815cb095708557014d42333d2cd87690107
Tor-Olsson <tor.olsson@gmail.com>
2018-05-24 15:38:39 +0200

    Påbörjat "hämta vänner"

--- src/pages/check-in/check-in.ts
+++ src/pages/check-in/check-in.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {FriendRequestPage} from "../friend-request/friend-request";
 import {AddFriendPage} from "../add-friend/add-friend";
+import {FriendProvider} from "../../providers/friend/friend";
 
 /**
  * Generated class for the CheckInPage page.
@@ -14,16 +15,28 @@ import {AddFriendPage} from "../add-friend/add-friend";
 @Component({
   selector: 'page-check-in',
   templateUrl: 'check-in.html',
+  providers: [FriendProvider]
 })
 export class CheckInPage {
 
+  onlineFriends : any;
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public friendProvider: FriendProvider) {
+    this.getOnlineUsers();
   }
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CheckInPage');
+  }
+
+  getOnlineUsers(){
+    this.friendProvider.getOnlineFriends("sven@sven.com")
+      .then(data => {
+        this.onlineFriends = data;
+        console.log(this.onlineFriends);
+      });
   }
 
   openPageAddFriend() {
